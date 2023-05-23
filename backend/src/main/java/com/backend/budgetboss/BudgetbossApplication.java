7a76205b018c25a0a2f075bcf10ce956ab54e8fe
itttz <iamcooled@gmail.com>
2023-05-23 15:53:43 -0500

    added backend files

---
+++ backend/src/main/java/com/backend/budgetboss/BudgetbossApplication.java
@@ -0,0 +1,13 @@
+package com.backend.budgetboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetbossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetbossApplication.class, args);
+	}
+
+}
