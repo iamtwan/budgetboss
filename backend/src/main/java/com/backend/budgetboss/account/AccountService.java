95b786b4b6d876934e9a8c7dba545d32b81e8d85
itttz <iamcooled@gmail.com>
2023-07-07 14:44:48 -0500

    Revert "added ApplicationConversionService to WebMvcConfigure config, added controller for Accounts, and added getAccountsByType request method"
    This reverts commit 13381117e078f5e1230b4e1473ff14ea7b6c3a4e.

--- backend/src/main/java/com/backend/budgetboss/account/AccountService.java
+++ backend/src/main/java/com/backend/budgetboss/account/AccountService.java
@@ -1,15 +1,9 @@
 package com.backend.budgetboss.account;
 
-import com.backend.budgetboss.account.dto.AccountResponseDTO;
 import com.backend.budgetboss.item.Item;
-import com.backend.budgetboss.manualaccount.ManualAccountType;
-import com.backend.budgetboss.user.User;
 import java.io.IOException;
-import java.util.List;
 
 public interface AccountService {
 
   void createAccounts(Item item) throws IOException;
-
-  List<AccountResponseDTO> getAccountsByType(User user, ManualAccountType type);
 }
