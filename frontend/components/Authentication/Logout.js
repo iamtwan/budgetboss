e50fa1c1005b62ad34330c381054a8a54a6c63d5
twan <thisiscasillas@gmail.com>
2023-08-15 17:37:30 -0700

    Updated navbar icon and text

--- frontend/components/Authentication/Logout.js
+++ frontend/components/Authentication/Logout.js
@@ -22,7 +22,7 @@ const Logout = () => {
     };
 
     return (
-        <Button id='logout-btn' className='btn btn-sm p-2 fw-semibold ms-2' onClick={handleLogout}>Logout</Button>
+        <Button id='logout-btn' className='btn btn-sm p-2 fw-semibold ms-2' onClick={handleLogout}><i className='bi bi-box-arrow-right'></i></Button>
     );
 };
 
